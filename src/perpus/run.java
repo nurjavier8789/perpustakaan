d8c2b66852d44c35ab3158908f6a775f570b3ff6
nurjavier8789 <nurjavierprasetyo8789@gmail.com>
2023-05-03 11:30:07 +0700

    Update run.java
    - Disabling Petugas Mode
    - Adding more loops!

--- src/perpus/run.java
+++ src/perpus/run.java
@@ -14,22 +14,29 @@ public class run {
         Scanner input = new Scanner(System.in);
         
         String last = "";
+        String init = "";
         
         System.out.println("Selamat datang di Perpustakaan!");
-        System.out.print("Masuk sebagai? ('S'iswa/'P'etugas)\n> ");
-        String pil = input.next();
         
         do {
-            if (pil.equalsIgnoreCase("s")) {
-                pinjam.prosesPinjam(siswa, buku);
-            } else if (pil.equalsIgnoreCase("p")) {
-                petugas.modePetugas();
-            } else {
-                System.out.println("\nTidak ada opsinya :D\nBye~");
-            }
+            System.out.print("Masuk sebagai? ('S'iswa/'P'etugas)\n> ");
+            String pil = input.next();
+            do {
+                if (pil.equalsIgnoreCase("s")) {
+                    pinjam.prosesPinjam(siswa, buku);
+                    System.out.print("\nApakah ingin menggunakan akun lainnya? (y/n)\n> ");
+                    last = input.next();
+                } else if (pil.equalsIgnoreCase("p")) {
+                    System.out.println("Maaf untuk saat ini belum tersedia...");
+                } else {
+                    System.out.println("\nTidak ada opsinya :D\nBye~");
+                }
+            } while (last.equalsIgnoreCase("y"));
             
-            System.out.print("Apakah ingin menggunakan akun lainnya? (y/n)\n> ");
-            last = input.next();
-        } while (last.equalsIgnoreCase("y"));
+            System.out.print("\nIngin Berganti mode? (y/n)\n> ");
+            init = input.next();
+        } while (init.equalsIgnoreCase("y"));
+        
+        System.out.println("\n\nSee next time!!");
     }
 }
